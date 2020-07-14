60c74fb5335d7027d5eb0bba11607b086cbf5f7c
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-07-14 15:39:39 +0900

    m

--- server.py
+++ server.py
@@ -45,9 +45,10 @@ def render3D():
     count = 0
     while proc.poll() is None:  # Check the the child process is still running
       data = proc.stdout.read(1)  # Note: it reads as binary, not text
-      if data!= b" " and data!= b"" and data is not None:
+      if data!= " " and data!= "" and data is not None:
         count += 1
         print(data)
+        print(type(data))
     print(count)
     #msg, err = p.communicate()
     #print(msg)
