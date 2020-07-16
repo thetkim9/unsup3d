7df10db51c74c060839189effd95729bbb47064f
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-07-16 11:01:20 +0900

    m

--- server.py
+++ server.py
@@ -125,6 +125,7 @@ def progress(user_id):
     global progressRates
     if user_id not in progressRates:
       progressRates[user_id] = 0
+    print(progressRates[user_id])
     return str(progressRates[user_id])
 
 @app.errorhandler(413)
