6b8e9e8e4dddb19f2d561d0dfeb8b029e4528c5e
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-07-16 10:55:15 +0900

    m

--- static/request.js
+++ static/request.js
@@ -1,7 +1,7 @@
 function check_progress(task_id, progress_bar) {
     function worker() {
       $.get('progress/' + task_id, function(data) {
-          alert(data);
+      document.body.innerHTML += data;
           if (progress < 100) {
               document.body.innerHTML += progress;
               progress_bar.set_progress(progress);
